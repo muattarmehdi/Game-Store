af525d33671ec2a360c7ef2c053c0569c072edd5
muattarmehdi <muattermehdi@gmail.com>
2023-12-10 00:26:53 +0500

    Toggle Color Switch

---
+++ src/components/ColorModeToggle.tsx
@@ -0,0 +1,18 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeToggle = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text> Dark </Text>
+    </HStack>
+  );
+};
+
+export default ColorModeToggle;
